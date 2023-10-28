9bff35f9212675a0bab701b27e8931e4dffb15a5
Sultan Iman <sultan.imanhodjaev@gmail.com>
2023-10-28 14:46:39 +0200

    qommandaloo interfejsin qoşuu

--- cmd/root.go
+++ cmd/root.go
@@ -1,17 +1,27 @@
 package cmd
 
 import (
+	"bufio"
+	"github.com/kyrgyz-nlp/qtr/text"
 	"github.com/spf13/cobra"
+	"log"
+	"os"
 )
 
 var rootCmd = cobra.Command{
 	Use:   "qtr",
-	Short: "Kirill tekstterdi Qyrgyzdyn latyn tamgasyna transliterlöö",
-	RunE: func(cmd *cobra.Command, args []string) error {
-		return nil
+	Long:  "cat <FILE> | qtr",
+	Short: "Kirill tekstterdi Qyrğyz-latyn tamğasyna transliterlöö",
+	Run: func(cmd *cobra.Command, args []string) {
+		scanner := bufio.NewScanner(os.Stdin)
+		scanner.Split(text.ScanLinesWithLF)
+		converter := text.NewConverter(scanner)
+		converter.Convert(os.Stdout)
 	},
 }
 
-func init() {
-
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		log.Fatal(err)
+	}
 }
--- main.go
+++ main.go
@@ -1,1 +1,7 @@
 package main
+
+import "github.com/kyrgyz-nlp/qtr/cmd"
+
+func main() {
+	cmd.Execute()
+}
